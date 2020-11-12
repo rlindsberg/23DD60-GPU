43bd8fb99773f2d6d7553e3128111fd6c5e8723b
rlindsberg <roderick@karlemstrand.com>
2020-11-12 14:22:15 +0100

    CPU 233289 GPU 91. Done Q2

--- Assignment II CUDA Basics/2 SAXPY/exercise_2.cu
+++ Assignment II CUDA Basics/2 SAXPY/exercise_2.cu
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#define N 20
-#define TPB 20
+#include <limits>
+#include <iostream>
+#include <chrono>
+#define N 256
+#define TPB 256
 
 __device__ float plus(float a, float b)
 {
@@ -11,10 +14,10 @@ __global__ void distanceKernel(float *d_out, float *d_x, float *d_y)
 {
 
     int i = blockIdx.x*blockDim.x + threadIdx.x;
-    printf("Adding %f and %f \n", d_x[i], d_y[i]);
+    //printf("Adding %f and %f \n", d_x[i], d_y[i]);
     d_out[i] = d_x[i] + d_y[i];
-    printf("Finished from thread %d \n", i);
-    printf("%f.\n", d_out[i]);
+    //printf("Finished from thread %d \n", i);
+    //printf("%f.\n", d_out[i]);
 }
 
 int main()
@@ -25,27 +28,34 @@ int main()
     float *d_y = 0;
 
     // Allocate device memory to store the output array
-    cudaMalloc(&d_out, 20*sizeof(float));
-    cudaMalloc(&d_x, 20*sizeof(float));
-    cudaMalloc(&d_y, 20*sizeof(float));
+    cudaMalloc(&d_out, 100000000*sizeof(float));
+    cudaMalloc(&d_x, 100000000*sizeof(float));
+    cudaMalloc(&d_y, 100000000*sizeof(float));
 
-    float *x = (float*) malloc(20*sizeof(float)); for(int i=0; i < 20; i++) x[i] = 100.0;
-    float *y = (float*) malloc(20*sizeof(float)); for(int i=0; i < 20; i++) y[i] = 100.1;
+    float *x = (float*) malloc(100000000*sizeof(float)); for(int i=0; i < 100000000; i++) x[i] = 100.0;
+    float *y = (float*) malloc(100000000*sizeof(float)); for(int i=0; i < 100000000; i++) y[i] = 100.1;
 
     // CPU add
-    for (int i=0;i < 20;i++) {
+    auto start = std::chrono::steady_clock::now();
+    for (int i=0;i < 100000000;i++) {
         float res = x[i] + y[i];
     //    printf("%f\n", res);
     }
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "CPU " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
 
-    cudaMemcpy(d_x, x, 20*sizeof(float),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, 20*sizeof(float),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, 100000000*sizeof(float),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, 100000000*sizeof(float),cudaMemcpyHostToDevice);
 
     //my_kernel <<<Dg, Db>>>(arg1, arg2, ...)
     // Dg = number of thread blocks
     // Db = number of threads per block
     // The total number of threads is Dg*Db.
-    distanceKernel<<<N/TPB, TPB>>>(d_out, d_x, d_y);
+    start = std::chrono::steady_clock::now();
+    distanceKernel<<<(N+TPB-1)/TPB, TPB>>>(d_out, d_x, d_y);
+    end = std::chrono::steady_clock::now();
+
+    std::cout << "GPU " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
 
     // Free the memory
     cudaFree(d_out);
