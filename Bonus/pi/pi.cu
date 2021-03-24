b238910cff52cf5175d00914f4b552b88c5ee365
rlindsberg <roderick@karlemstrand.com>
2021-03-24 14:37:04 +0100

    fix indent

--- Bonus/pi/pi.cu
+++ Bonus/pi/pi.cu
@@ -10,21 +10,21 @@
 
 
 __global__ void gpu_random(curandState *states, float *device_pi_estimates) {
-	int id = threadIdx.x + blockDim.x * blockIdx.x;
+    int id = threadIdx.x + blockDim.x * blockIdx.x;
     int seed = id; // different seed per thread
-	float x, y, z;
-	int p_in_circle = 0;
+    float x, y, z;
+    int p_in_circle = 0;
 
-	curand_init(seed, id, 0, &states[id]);  // 	Initialize CURAND          
+    curand_init(seed, id, 0, &states[id]);  // 	Initialize CURAND
 
-	for(int i = 0; i < TRIALS_PER_THREAD; i++) {
-		x = curand_uniform (&states[id]);
+    for(int i = 0; i < TRIALS_PER_THREAD; i++) {
+        x = curand_uniform (&states[id]);
         y = curand_uniform (&states[id]);
         z = x * x + y * y;
         if (z <= 1.0) {
             p_in_circle ++;
         }
-	}
+    }
 
     device_pi_estimates[id] = 4.0 * (float)p_in_circle / (float)TRIALS_PER_THREAD;
 }
@@ -38,7 +38,7 @@ int main(int argc, char const *argv[]) {
     float *d_pi, *pi;
     cudaMalloc(&d_pi, sizeof(float) * N);
     pi = (float*)malloc(sizeof(float) * N);
-    
+
     // call kernel
     gpu_random <<<(N+TPB-1)/TPB, TPB>>>(dev_random, d_pi);
 
@@ -52,7 +52,7 @@ int main(int argc, char const *argv[]) {
     float sum_pi = 0;
     float ave_pi = 0;
     for (int i = 0; i < N; i++) {
-      sum_pi += pi[i];
+        sum_pi += pi[i];
     }
     ave_pi = sum_pi / float(N);
 
